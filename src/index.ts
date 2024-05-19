f04c006c2bd93c7e2be69309d1503e1979991b32
Wiktoria <wstefaniak@student.agh.edu.pl>
2024-05-19 13:33:01 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as BABYLON from "babylonjs";
 import { AbstractMesh } from "babylonjs";
+import { Environment } from "../environment";
 
 // Some global handles for graphics
 const theCanvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
@@ -39,11 +40,40 @@ var createScene = async function () {
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
     sphere.position.y = 1;
        
-    const env = scene.createDefaultEnvironment();
-    if(!env) return null;
+    const environment = new Environment(scene, engine);
+    environment.init();
+    // Create a floor in the scene and position it to the center
+    var gymFloor = MeshBuilder.CreateGround("ground", { width: 60, height: 60 }, scene);
+    gymFloor.position = new Vector3(0, -3.5, 0);
 
+    // Create wood materials and texture in the scene
+    var woodMaterial = new StandardMaterial("woodMaterial", scene);
+    var woodTexture = new WoodProceduralTexture("text", 1024, scene);
+
+    // Adjust the texture to look more realistic 
+    woodTexture.ampScale = 80.0;
+
+    // Apply the texture to the material
+    woodMaterial.diffuseTexture = woodTexture;
+
+    // Apply the material to the gym floor mesh object
+    gymFloor.material = woodMaterial;
+
+    // Add physics that simulates the ground
+    gymFloor.physicsImpostor = new PhysicsImpostor(gymFloor, PhysicsImpostor.PlaneImpostor, { mass: 0, restitution: 1 }, scene);
+        if(!env) return null;
+    // Create PhotoDome with a .png image and add it to the scene
+    var dome = new PhotoDome(
+        "mydome",
+        "https://sjanlassets.blob.core.windows.net/assets/Looney-Court.png",
+        {
+            resolution: 32,
+            size: 100
+        },
+        scene
+    );    
     await scene.createDefaultXRExperienceAsync({
-        floorMeshes: [env.ground as AbstractMesh],
+        floorMeshes: [gymFloor],
     });
 
     return scene;
